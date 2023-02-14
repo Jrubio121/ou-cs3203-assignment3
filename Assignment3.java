e948e501f3b0ed4bfabea7561a1c5e6dd12417d0
Jrubio121 <98114036+Jrubio121@users.noreply.github.com>
2023-02-13 19:05:37 -0600

    Initial commit with the addNumbers method
    First commit with a method that adds the numbers in an array.

---
+++ Assignment3.java
@@ -0,0 +1,15 @@
+
+public class Assignment3 {
+	
+	// Method that adds numbers in an array
+	public static int addNumbers(int[] numbers) {
+		
+		int result = 0;
+		for(int i = 0; i < numbers.length; ++i) {
+			result = result + numbers[i];
+		}
+		
+		return result;
+	}
+	
+}
